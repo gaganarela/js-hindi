4125a2e0f9dee896dbff8d4039ca18b777095c7f
gaganarela <gaganarela1982@gmail.com>
2025-10-03 16:59:00 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3) //not allowed in nodejs,we are not in browser.....
+console.log(3+3)
+console.log("gaagagb")
+//number
+//string 
+//bigInt
+//boolean
+//null
+//undefined
+//symbol //uniqueness
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
+
+
+
