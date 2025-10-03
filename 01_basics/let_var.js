ca8b6504d7dfca1d00b23fb9164d351b496182b8
gaganarela <gaganarela1982@gmail.com>
2025-10-03 16:59:41 +0000

    let

---
+++ 01_basics/let_var.js
@@ -0,0 +1,8 @@
+let accountEmail="gagan@google.com"
+const accountId="323432"
+var accountPassword="2452342"
+accountCity="jaipur"
+// do not use var bcz it depends on scope and other things
+// both var and let are of same use
+// const value does not get changed
+console.table([accountEmail,accountId,accountPassword,accountCity])
