e764ea6255b93f1e32d995504faf34cc89f44193
gaganarela <gaganarela1982@gmail.com>
2025-10-03 19:47:09 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,4 @@
+const str="RAM"
+console.log(`${str} is a good boy`)//string interpolation using backticks
+const gamename=new String('shyam')//object datatype now.....so different methods can be called on strings now
+console.log(gamename.length)
